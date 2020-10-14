35c37534dafd1749bb67195a972d516e83e2a591
Mohamadfazel Amini <61580306+XMFAZ@users.noreply.github.com>
2020-10-14 19:31:36 +0330

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,40 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Create Identity'
+
+version '1.0.0'
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/cs.lua',
+	'locales/pl.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/cs.lua',
+	'locales/pl.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/js/script.js',
+	'html/css/style.css',
+	'html/img/header_identity.png',
+	'html/img/background.jpg'
+}
+
+dependency 'es_extended'
